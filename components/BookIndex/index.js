eef2030489bdb676add466917e12ec2322a10054
InnovativeCoder <aashitsingh2010@gmail.com>
2022-01-28 22:29:33 -0500

    UPdated image

--- components/BookIndex/index.js
+++ components/BookIndex/index.js
@@ -5,7 +5,7 @@ export default function index() {
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-9 my-10">
                <div className="lg:flex">
                     <div>
-                         <img className="mx-auto  rounded-xl lg:w-1/3 shadow-xl shadow-pink-500/70" src="IndexPage.png" alt="Index of the Book" />
+                         <img className="mx-auto  rounded-xl lg:w-1/3 shadow-xl shadow-pink-500/70" src="indexPage.png" alt="Index of the Book" />
                     </div>
                     <div className='hidden'>
                          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
